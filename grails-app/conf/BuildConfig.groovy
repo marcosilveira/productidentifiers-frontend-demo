cdb3f65d7fe2572328ffec6e76b54db0f0e9ba99
marcosilveira <vastar@globo.com>
2016-05-15 18:14:37 -0300

    Update BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,6 +22,7 @@ grails.project.dependency.resolution = {
 
         mavenLocal()
         mavenCentral()
+        mavenRepo "http://repo.grails.org/grails/plugins/"
         grailsRepo "https://grails.org/plugins"
     }
 
